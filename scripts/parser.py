f0faa3084ed084eb8d22e03b5d6f1564410bfbaa
ychalier <yohan.chalier@telecom-paristech.fr>
2020-03-25 13:47:49 +0100

    Enhanced notes parser: fixed checkmate evaluations, added Komodo engine support, fixed en-passant in FENs, added note export

--- scripts/parser.py
+++ scripts/parser.py
@@ -2,10 +2,11 @@
 
 This script takes a text file as input and builds a JSON representation of the
 openings repertoire, meant for upload on the web application. It requires a
-stockfish installation.
+UCI chess engine installation.
 
 See https://github.com/ychalier/django-chess for more details.
-To get the engine, go to https://stockfishchess.org/.
+To get the engine, you may go to https://stockfishchess.org/ or
+https://komodochess.com/.
 """
 
 import re
@@ -41,13 +42,27 @@ class Node:
         """Serialization"""
         return {
             "uid": self.uid,
-            "fen": self.board.fen(),
+            "fen": self.board.fen(en_passant="fen"),
             "ev": self.evaluation,
             "cmt": self.comment,
             "p": self.parent,
             "lbl": self.label,
             "line": self.line,
         }
+
+    def depth(self):
+        if self.board.turn == chess.WHITE:
+            return 2 * self.board.fullmove_number
+        return 2 * self.board.fullmove_number + 1
+
+    def to_line(self, indent=4):
+        if self.label == "":
+            return ""
+        string = " " * (self.depth() - 3) * indent
+        string += "%s (%s)" % (self.label, self.evaluation)
+        if self.comment != "":
+            string += " : " + self.comment
+        return string
 
 
 def parse_notes(filename):
@@ -96,8 +111,8 @@ def parse_notes(filename):
                         return None
                 child.note_line = i + 1
                 child.board.push(move)
-                child.label = "%d. " % fullmove_number
                 child.illustrative = "il. " in element
+                child.label = "%d. " % fullmove_number
                 if turn == chess.BLACK:
                     child.label += "... "
                 child.label += move_san
@@ -110,9 +125,19 @@ def parse_notes(filename):
     return nodes
 
 
-def evaluate(engine, board, depth):
+def evaluate(board, args):
     """Give the evaluation of a move"""
-    score = engine.analyse(board, chess.engine.Limit(depth=depth))["score"]
+    if board.is_stalemate():
+        return "0.00"
+    if board.is_checkmate():
+        if board.turn == chess.WHITE:
+            return "-M0"
+        return "M0"
+    engine = chess.engine.SimpleEngine.popen_uci(args.engine_exe)
+    engine.configure({"Contempt": args.contempt})
+    output = engine.analyse(board, chess.engine.Limit(depth=args.depth))
+    score = output["score"]
+    engine.quit()
     if score.is_mate():
         value = score.white().mate()
         if value >= 0:
@@ -124,11 +149,11 @@ def evaluate(engine, board, depth):
     return "%.2f" % (.01 * value)
 
 
-def evaluate_repertoire(repertoire, engine, depth, override):
+def evaluate_repertoire(repertoire, args):
     """Evaluate a set of nodes"""
     for node in tqdm.tqdm(repertoire):
-        if node.evaluation is None or override:
-            node.evaluation = evaluate(engine, node.board, depth)
+        if node.evaluation is None or args.override:
+            node.evaluation = evaluate(node.board, args)
 
 
 def main():
@@ -154,12 +179,35 @@ def main():
         "-d", "--depth",
         help="Evaluation depth",
         type=int,
-        default=20
+        default=18
+    )
+    parser.add_argument(
+        "-t", "--contempt",
+        help="Chess engine contempt option",
+        type=int,
+        default=0
     )
     parser.add_argument(
         "-v", "--override",
         help="Override evaluations",
         action="store_true"
+    )
+    parser.add_argument(
+        "-c", "--copy",
+        help="Make a copy of the notes with the new evaluations",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-cf", "--copy_file",
+        help="Path to copied notes file",
+        type=str,
+        default="notes.txt"
+    )
+    parser.add_argument(
+        "-ci", "--copy_indent",
+        help="Copied notes file indent",
+        type=int,
+        default=4
     )
     args = parser.parse_args()
     repertoire = parse_notes(args.note_txt)
@@ -187,14 +235,19 @@ def main():
             print("Found duplicates, please remove them.")
             return True
         print("Kept %d nodes after pruning." % len(pruned))
-        engine = chess.engine.SimpleEngine.popen_uci(args.engine_exe)
         print("Evaluating repertoire with engine '%s' at depth %d." %
               (args.engine_exe, args.depth))
-        evaluate_repertoire(pruned, engine, args.depth, args.override)
-        engine.quit()
+        evaluate_repertoire(pruned, args)
+        if args.copy:
+            print("Reproducing notes to '%s'" % args.copy_file)
+            with codecs.open(args.copy_file, "w", "utf8") as file:
+                for node in sorted(pruned, key=lambda node: node.uid):
+                    line = node.to_line(indent=args.copy_indent)
+                    if line != "":
+                        file.write(line + "\n")
         print("Writing output to '%s'." % args.output_json)
         with codecs.open(args.output_json, "w", "utf8") as file:
-            json.dump([node.to_dict() for node in pruned], file)
+            json.dump([node.to_dict() for node in pruned], file, indent=4, sort_keys=True)
     return True
 
 
